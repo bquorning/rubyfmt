bb87225a7178e65a585f85153fbc918c416dc18d
Sam Phippen <samphippen@googlemail.com>
2019-03-26 14:10:46 -0400

    Fixup string literal escape handling for many strings.
    Ruby is cancelled.

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,14 @@
 set -ex
 
 cp -r fixtures/2.5 fixtures/2.6
+STRING_LITERALS_EXPECTED=`ruby string_literals_stress_test.rb | md5`
+STRING_LITERALS_ACTUAL=`ruby --disable=gems src/rubyfmt.rb string_literals_stress_test.rb | ruby | md5`
+if [[ $STRING_LITERALS_EXPECTED != $STRING_LITERALS_ACTUAL ]]
+then
+    echo "string literals are broken"
+    exit 1
+fi
+
 for file in `ls fixtures/*_expected.rb` `ls fixtures/$(ruby -v | grep -o '\d\.\d')/*_expected.rb`
 do
     time ruby --disable=gems src/rubyfmt.rb `echo $file | sed s/expected/actual/` > /tmp/out.rb
