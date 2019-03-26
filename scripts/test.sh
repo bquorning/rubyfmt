f5a1d1f8f61f36c8ab1840c36b3f8227801d92fa
Sam Phippen <samphippen@googlemail.com>
2019-03-26 15:20:59 -0400

    Make md5 work accross platforms

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 set -ex
 
-cp -r fixtures/2.5 fixtures/2.6
-STRING_LITERALS_EXPECTED=`ruby string_literals_stress_test.rb | md5`
-STRING_LITERALS_ACTUAL=`ruby --disable=gems src/rubyfmt.rb string_literals_stress_test.rb | ruby | md5`
+f_md5() {
+    if [[ -z `which md5sum` ]]
+    then
+        md5
+    else
+        md5sum
+    fi
+}
+STRING_LITERALS_EXPECTED=`ruby string_literals_stress_test.rb | f_md5`
+STRING_LITERALS_ACTUAL=`ruby --disable=gems src/rubyfmt.rb string_literals_stress_test.rb | ruby | f_md5`
 if [[ $STRING_LITERALS_EXPECTED != $STRING_LITERALS_ACTUAL ]]
 then
     echo "string literals are broken"
     exit 1
 fi
 
+cp -r fixtures/2.5 fixtures/2.6
 for file in `ls fixtures/*_expected.rb` `ls fixtures/$(ruby -v | grep -o '\d\.\d')/*_expected.rb`
 do
     time ruby --disable=gems src/rubyfmt.rb `echo $file | sed s/expected/actual/` > /tmp/out.rb
